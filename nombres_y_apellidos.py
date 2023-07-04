f319e4e4333c41cc0f5cf11b2aaf112e1546d305
root <root@labvm.vm>
2023-07-04 21:35:23 +0000

    Agregado script de nombres y apellidos

---
+++ nombres_y_apellidos.py
@@ -0,0 +1,6 @@
+# Lista de nombres y apellidos de los integrantes del grupo
+        nombres_apellidos = ["Diego Araneda", "Jaime Astorga", "Vicente Palma", "Matias Silva"]
+
+# Imprimir la lista
+for nombre_apellido in nombres_apellidos:
+        print(nombre_apellido)
